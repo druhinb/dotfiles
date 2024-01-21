c868836b2750717c0c79b6b7196ec2e47db8e66b
druhinb <druhin.bhowal@outlook.com>
2024-01-20 20:00:22 -0700

    feat(nvim): improve diffview and add inline git conflict resolution

--- nvim/.config/nvim/lua/custom/plugins/diffview.lua
+++ nvim/.config/nvim/lua/custom/plugins/diffview.lua
@@ -4,8 +4,9 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
+    local actions = require("diffview.actions")
     require('diffview').setup({
-      diff_bin = 'diff', -- Use the system diff command
+      enhanced_diff_hl = true,
       use_icons = true,
       icons = {
         folder_closed = '',
@@ -18,37 +19,39 @@ return {
         remove = { text = '-' },
       },
       view = {
-        -- Configure the layout of the diff view
-        layout = {
-          {
-            type = 'diff',
-            win_opts = {
-              -- You can specify window options here
-            },
-          },
-          {
-            type = 'file_history',
-            win_opts = {
-              -- You can specify window options here
-            },
-          },
+        default = {
+          layout = 'diff2_horizontal',
+        },
+        merge_tool = {
+          layout = 'diff3_mixed',
+          disable_diagnostics = true,
+        },
+        file_history = {
+          layout = 'diff2_horizontal',
         },
       },
       file_panel = {
         listing_style = 'tree',
         tree_options = {
-          -- Options for the tree listing style
+          flatten_dirs = true,
+          folder_statuses = "only_folded",
         },
       },
       file_history_panel = {
         log_options = {
-          -- Options for the git log command
+          git = {
+            single_file = { diff_merges = "combined" },
+            multi_file = { diff_merges = "first-parent" },
+          },
         },
       },
-      merge_tool = {
-        -- Configuration for the merge tool
-        layout = 'diff3',
-        -- Other merge tool options
+      keymaps = {
+        view = {
+          { "n", "<leader>cc", actions.conflict_choose("ours"),   { desc = "Choose Current (Ours)" } },
+          { "n", "<leader>ci", actions.conflict_choose("theirs"), { desc = "Choose Incoming (Theirs)" } },
+          { "n", "<leader>cb", actions.conflict_choose("base"),   { desc = "Choose Base" } },
+          { "n", "<leader>ca", actions.conflict_choose("all"),    { desc = "Choose All (Both)" } },
+        },
       },
     })
   end,
