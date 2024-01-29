e679a0e928f626eeacb57e80e0974993000ea8f7
druhinb <druhin.bhowal@outlook.com>
2024-01-28 18:56:25 -0700

    feat(nvim): optimize keybindings and text object selections

--- nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
@@ -16,13 +19,115 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+
+      -- Incremental selection based on treesitter nodes
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
+
+      -- Treesitter textobjects configuration
+      textobjects = {
+        -- Selection textobjects (use with v, d, c, y, etc.)
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj
+          keymaps = {
+            -- Functions/Methods
+            ['af'] = { query = '@function.outer', desc = 'Around [F]unction' },
+            ['if'] = { query = '@function.inner', desc = 'Inside [F]unction' },
+
+            -- Classes
+            ['ac'] = { query = '@class.outer', desc = 'Around [C]lass' },
+            ['ic'] = { query = '@class.inner', desc = 'Inside [C]lass' },
+
+            -- Blocks (scopes)
+            ['ab'] = { query = '@block.outer', desc = 'Around [B]lock' },
+            ['ib'] = { query = '@block.inner', desc = 'Inside [B]lock' },
+
+            -- Comments
+            ['a/'] = { query = '@comment.outer', desc = 'Around comment' },
+            ['i/'] = { query = '@comment.inner', desc = 'Inside comment' },
+
+            -- Assignments
+            ['a='] = { query = '@assignment.outer', desc = 'Around assignment' },
+            ['i='] = { query = '@assignment.inner', desc = 'Inside assignment' },
+            ['l='] = { query = '@assignment.lhs', desc = '[L]eft side of assignment' },
+            ['r='] = { query = '@assignment.rhs', desc = '[R]ight side of assignment' },
+
+            -- Include surrounding whitespace
+            include_surrounding_whitespace = true,
+          },
+
+          -- Swap textobjects (swap function parameters, etc.)
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>an'] = { query = '@parameter.inner', desc = 'Swap [A]rg with [N]ext' },
+            },
+            swap_previous = {
+              ['<leader>ap'] = { query = '@parameter.inner', desc = 'Swap [A]rg with [P]rev' },
+            },
+          },
+
+          -- Move to next/previous textobject
+          move = {
+            enable = true,
+            set_jumps = true, -- Add to jumplist
+            goto_next_start = {
+              [']f'] = { query = '@function.outer', desc = 'Next [F]unction start' },
+              [']k'] = { query = '@class.outer', desc = 'Next [K]lass start' },
+              [']r'] = { query = '@return.outer', desc = 'Next [R]eturn' },
+              [']/'] = { query = '@comment.outer', desc = 'Next comment' },
+              [']='] = { query = '@assignment.outer', desc = 'Next assignment' },
+            },
+            goto_next_end = {
+              [']F'] = { query = '@function.outer', desc = 'Next [F]unction end' },
+              [']K'] = { query = '@class.outer', desc = 'Next [K]lass end' },
+              [']O'] = { query = '@loop.outer', desc = 'Next l[O]op end' },
+            },
+            goto_previous_start = {
+              ['[f'] = { query = '@function.outer', desc = 'Prev [F]unction start' },
+              ['[k'] = { query = '@class.outer', desc = 'Prev [K]lass start' },
+              ['[r'] = { query = '@return.outer', desc = 'Prev [R]eturn' },
+              ['[/'] = { query = '@comment.outer', desc = 'Prev comment' },
+              ['[='] = { query = '@assignment.outer', desc = 'Prev assignment' },
+            },
+            goto_previous_end = {
+              ['[F'] = { query = '@function.outer', desc = 'Prev [F]unction end' },
+              ['[K'] = { query = '@class.outer', desc = 'Prev [K]lass end' },
+              ['[O'] = { query = '@loop.outer', desc = 'Prev l[O]op end' },
+            },
+          },
+
+          -- LSP interop for smarter jumping
+          lsp_interop = {
+            enable = true,
+            border = 'rounded',
+            floating_preview_opts = {},
+            peek_definition_code = {
+              ['<leader>pf'] = { query = '@function.outer', desc = '[P]eek [F]unction definition' },
+              ['<leader>pk'] = { query = '@class.outer', desc = '[P]eek [K]lass definition' },
+            },
+          },
+        },
+      },
+      config = function(_, opts)
+        require('nvim-treesitter.configs').setup(opts)
+
+        -- Repeat movement with ; and ,
+        local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+        -- Make ; and , repeat the last treesitter textobject move
+        vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next, { desc = 'Repeat last move (next)' })
+        vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous, { desc = 'Repeat last move (previous)' })
+      end,
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/kickstart/plugins/which-key.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/which-key.lua
@@ -83,22 +83,116 @@ return {
 
       -- Document existing key chains
       spec = {
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>t', group = '[T]oggle/Tabs' },
+        -- Leader groups
+        { '<leader>a', group = '[A]rgument swap' },
+        { '<leader>b', group = '[B]uffer' },
+        { '<leader>c', group = '[C]onflict' },
+        { '<leader>d', group = '[D]iagnostics' },
         { '<leader>g', group = '[G]it' },
         { '<leader>h', group = 'Git [H]unk / Resize Left', mode = { 'n', 'v' } },
         { '<leader>l', group = '[L]ayout / Resize Right' },
-        { '<leader>c', group = '[C]onflict' },
-        { '<leader>d', group = '[D]iagnostic' },
-        { '<leader>w', group = '[W]rite' },
-        { '<C-w>', group = 'Window' },
-        { 'g', group = '[G]o' },
-        { 'z', group = '[Z] Fold' },
+        { '<leader>o', group = '[O]rganize' },
+        { '<leader>p', group = '[P]eek Definition' },
+        { '<leader>q', group = '[Q]uickfix' },
+        { '<leader>r', group = '[R]ename/Refactor' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle/Tabs' },
+        { '<leader>w', group = '[W]rite/Workspace' },
+
+        -- Vim motion groups
+        { '<C-w>', group = '[W]indow' },
+        { 'g', group = '[G]o to' },
+        { 'z', group = '[Z] Fold/Scroll' },
+        { 'm', group = 'Set [M]ark' },
+        { "'", group = "Jump to [']Mark" },
+        { '`', group = 'Jump to [`]Mark (exact)' },
+        { '"', group = '["]Registers' },
+
+        -- Navigation groups
         { '[', group = '[P]revious ...' },
         { ']', group = '[N]ext ...' },
-        { 'm', group = '[M]ark' },
-        { "'", group = '[J]ump to Mark' },
-        { '"', group = '[R]egister' },
+
+        -- Quickfix navigation
+        { '[q', desc = 'Prev [Q]uickfix item' },
+        { ']q', desc = 'Next [Q]uickfix item' },
+        { '[Q', desc = 'First [Q]uickfix item' },
+        { ']Q', desc = 'Last [Q]uickfix item' },
+
+        -- Location list navigation
+        { '[l', desc = 'Prev [L]ocation list item' },
+        { ']l', desc = 'Next [L]ocation list item' },
+        { '[L', desc = 'First [L]ocation list item' },
+        { ']L', desc = 'Last [L]ocation list item' },
+
+        -- Tab navigation
+        { '[t', desc = 'Prev [T]ab' },
+        { ']t', desc = 'Next [T]ab' },
+        { '[T', desc = 'First [T]ab' },
+        { ']T', desc = 'Last [T]ab' },
+
+        -- Diagnostic navigation
+        { '[d', desc = 'Prev [D]iagnostic' },
+        { ']d', desc = 'Next [D]iagnostic' },
+
+        -- Treesitter textobject navigation (defined in treesitter.lua)
+        { '[f', desc = 'Prev [F]unction start' },
+        { ']f', desc = 'Next [F]unction start' },
+        { '[F', desc = 'Prev [F]unction end' },
+        { ']F', desc = 'Next [F]unction end' },
+        { '[k', desc = 'Prev [K]lass start' },
+        { ']k', desc = 'Next [K]lass start' },
+        { '[K', desc = 'Prev [K]lass end' },
+        { ']K', desc = 'Next [K]lass end' },
+        { '[a', desc = 'Prev [A]rgument' },
+        { ']a', desc = 'Next [A]rgument' },
+        { '[o', desc = 'Prev l[O]op' },
+        { ']o', desc = 'Next l[O]op' },
+        { '[r', desc = 'Prev [R]eturn' },
+        { ']r', desc = 'Next [R]eturn' },
+        { '[/', desc = 'Prev comment' },
+        { ']/', desc = 'Next comment' },
+        { '[=', desc = 'Prev assignment' },
+        { ']=', desc = 'Next assignment' },
+
+        -- Spell navigation
+        { '[s', desc = 'Prev mi[S]spelled word' },
+        { ']s', desc = 'Next mi[S]spelled word' },
+
+        -- Git/Diff change navigation (gitsigns)
+        { '[c', desc = 'Prev [C]hange hunk' },
+        { ']c', desc = 'Next [C]hange hunk' },
+
+        -- Method navigation (built-in)
+        { '[m', desc = 'Prev [M]ethod start' },
+        { ']m', desc = 'Next [M]ethod start' },
+        { '[M', desc = 'Prev [M]ethod end' },
+        { ']M', desc = 'Next [M]ethod end' },
+
+        -- Section navigation
+        { '[[', desc = 'Prev section start' },
+        { ']]', desc = 'Next section start' },
+        { '[]', desc = 'Prev section end' },
+        { '][', desc = 'Next section end' },
+
+        -- Fold navigation
+        { '[z', desc = 'Start of current fold' },
+        { ']z', desc = 'End of current fold' },
+        { 'zj', desc = 'Next fold' },
+        { 'zk', desc = 'Prev fold' },
+
+        -- Empty line/paragraph navigation
+        { '{', desc = 'Prev empty line/paragraph' },
+        { '}', desc = 'Next empty line/paragraph' },
+
+        -- Brace/paren navigation
+        { '[(', desc = 'Prev unmatched (' },
+        { '])', desc = 'Next unmatched )' },
+        { '[{', desc = 'Prev unmatched {' },
+        { ']}', desc = 'Next unmatched }' },
+
+        -- Operator-pending textobject descriptions
+        { 'a', group = '[A]round (textobject)', mode = { 'o', 'x' } },
+        { 'i', group = '[I]nside (textobject)', mode = { 'o', 'x' } },
       },
       filter = function(mapping)
         -- return true to include the mapping, false to exclude it
