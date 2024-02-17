da747042c8bb43cb7b37a5a67739d7a515e5f103
druhinb <druhin.bhowal@outlook.com>
2024-02-17 00:31:26 -0700

    fix(nvim): resolve tree-sitter configuration issues

--- nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -39,95 +39,94 @@ return {
           lookahead = true, -- Automatically jump forward to textobj
           keymaps = {
             -- Functions/Methods
-            ['af'] = { query = '@function.outer', desc = 'Around [F]unction' },
-            ['if'] = { query = '@function.inner', desc = 'Inside [F]unction' },
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
 
             -- Classes
-            ['ac'] = { query = '@class.outer', desc = 'Around [C]lass' },
-            ['ic'] = { query = '@class.inner', desc = 'Inside [C]lass' },
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
 
             -- Blocks (scopes)
-            ['ab'] = { query = '@block.outer', desc = 'Around [B]lock' },
-            ['ib'] = { query = '@block.inner', desc = 'Inside [B]lock' },
+            ['ab'] = '@block.outer',
+            ['ib'] = '@block.inner',
 
             -- Comments
-            ['a/'] = { query = '@comment.outer', desc = 'Around comment' },
-            ['i/'] = { query = '@comment.inner', desc = 'Inside comment' },
+            ['a/'] = '@comment.outer',
+            ['i/'] = '@comment.inner',
 
             -- Assignments
-            ['a='] = { query = '@assignment.outer', desc = 'Around assignment' },
-            ['i='] = { query = '@assignment.inner', desc = 'Inside assignment' },
-            ['l='] = { query = '@assignment.lhs', desc = '[L]eft side of assignment' },
-            ['r='] = { query = '@assignment.rhs', desc = '[R]ight side of assignment' },
-
-            -- Include surrounding whitespace
-            include_surrounding_whitespace = true,
+            ['a='] = '@assignment.outer',
+            ['i='] = '@assignment.inner',
+            ['l='] = '@assignment.lhs',
+            ['r='] = '@assignment.rhs',
           },
+          include_surrounding_whitespace = true,
+        },
 
-          -- Swap textobjects (swap function parameters, etc.)
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>an'] = { query = '@parameter.inner', desc = 'Swap [A]rg with [N]ext' },
-            },
-            swap_previous = {
-              ['<leader>ap'] = { query = '@parameter.inner', desc = 'Swap [A]rg with [P]rev' },
-            },
+        -- Swap textobjects (swap function parameters, etc.)
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>an'] = '@parameter.inner',
           },
-
-          -- Move to next/previous textobject
-          move = {
-            enable = true,
-            set_jumps = true, -- Add to jumplist
-            goto_next_start = {
-              [']f'] = { query = '@function.outer', desc = 'Next [F]unction start' },
-              [']k'] = { query = '@class.outer', desc = 'Next [K]lass start' },
-              [']r'] = { query = '@return.outer', desc = 'Next [R]eturn' },
-              [']/'] = { query = '@comment.outer', desc = 'Next comment' },
-              [']='] = { query = '@assignment.outer', desc = 'Next assignment' },
-            },
-            goto_next_end = {
-              [']F'] = { query = '@function.outer', desc = 'Next [F]unction end' },
-              [']K'] = { query = '@class.outer', desc = 'Next [K]lass end' },
-              [']O'] = { query = '@loop.outer', desc = 'Next l[O]op end' },
-            },
-            goto_previous_start = {
-              ['[f'] = { query = '@function.outer', desc = 'Prev [F]unction start' },
-              ['[k'] = { query = '@class.outer', desc = 'Prev [K]lass start' },
-              ['[r'] = { query = '@return.outer', desc = 'Prev [R]eturn' },
-              ['[/'] = { query = '@comment.outer', desc = 'Prev comment' },
-              ['[='] = { query = '@assignment.outer', desc = 'Prev assignment' },
-            },
-            goto_previous_end = {
-              ['[F'] = { query = '@function.outer', desc = 'Prev [F]unction end' },
-              ['[K'] = { query = '@class.outer', desc = 'Prev [K]lass end' },
-              ['[O'] = { query = '@loop.outer', desc = 'Prev l[O]op end' },
-            },
+          swap_previous = {
+            ['<leader>ap'] = '@parameter.inner',
           },
+        },
 
-          -- LSP interop for smarter jumping
-          lsp_interop = {
-            enable = true,
-            border = 'rounded',
-            floating_preview_opts = {},
-            peek_definition_code = {
-              ['<leader>pf'] = { query = '@function.outer', desc = '[P]eek [F]unction definition' },
-              ['<leader>pk'] = { query = '@class.outer', desc = '[P]eek [K]lass definition' },
-            },
+        -- Move to next/previous textobject
+        move = {
+          enable = true,
+          set_jumps = true, -- Add to jumplist
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']k'] = '@class.outer',
+            [']r'] = '@return.outer',
+            [']/'] = '@comment.outer',
+            [']='] = '@assignment.outer',
+          },
+          goto_next_end = {
+            [']F'] = '@function.outer',
+            [']K'] = '@class.outer',
+            [']O'] = '@loop.outer',
+          },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[k'] = '@class.outer',
+            ['[r'] = '@return.outer',
+            ['[/'] = '@comment.outer',
+            ['[='] = '@assignment.outer',
+          },
+          goto_previous_end = {
+            ['[F'] = '@function.outer',
+            ['[K'] = '@class.outer',
+            ['[O'] = '@loop.outer',
+          },
+        },
+
+        -- LSP interop for smarter jumping
+        lsp_interop = {
+          enable = true,
+          border = 'rounded',
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ['<leader>pf'] = '@function.outer',
+            ['<leader>pk'] = '@class.outer',
           },
         },
       },
-      config = function(_, opts)
-        require('nvim-treesitter.configs').setup(opts)
-
-        -- Repeat movement with ; and ,
-        local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
-
-        -- Make ; and , repeat the last treesitter textobject move
-        vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next, { desc = 'Repeat last move (next)' })
-        vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous, { desc = 'Repeat last move (previous)' })
-      end,
     },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- Repeat movement with ; and ,
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+      -- Make ; and , repeat the last treesitter textobject move
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next, { desc = 'Repeat last move (next)' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous,
+        { desc = 'Repeat last move (previous)' })
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
