35a90f1c003b7de36b4b645db378b85fe6d39054
druhinb <druhin.bhowal@outlook.com>
2024-10-27 21:00:59 -0700

    style(nvim): apply rounded borders to fzf-lua and treesitter context

--- nvim/.config/nvim/lua/custom/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter-context.lua
@@ -4,7 +4,21 @@ return {
   config = function()
     require('treesitter-context').setup {
       enable = true,
-      max_lines = 3,
+      max_lines = 3, -- How many lines to show at the top
+      trim_scope = 'outer',
+      patterns = {
+        -- Match all languages
+        default = {
+          'class',
+          'function',
+          'method',
+          'for', -- Show for loops
+          'while', -- Show while loops
+          'if', -- Show if statements
+          'switch',
+          'case',
+        },
+      },
     }
   end,
 }
