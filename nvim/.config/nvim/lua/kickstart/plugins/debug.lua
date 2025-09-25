329add34be470f651ce581049b1c8b147f9cf87b
druhinb <druhin.bhowal@outlook.com>
2025-09-25 07:57:53 -0700

    fix(nvim): correct codelldb extension path and arguments

--- nvim/.config/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/debug.lua
@@ -299,13 +299,14 @@ return {
       local codelldb = mason_registry.get_package 'codelldb'
       local extension_path = codelldb:get_install_path() .. '/extension/'
       local codelldb_path = extension_path .. 'adapter/codelldb'
+      local liblldb_path = extension_path .. 'lldb/lib/liblldb' .. (vim.fn.has 'mac' == 1 and '.dylib' or '.so')
 
       dap.adapters.codelldb = {
         type = 'server',
         port = '${port}',
         executable = {
           command = codelldb_path,
-          args = { '--port', '${port}' },
+          args = { '--port', '${port}', '--liblldb', liblldb_path },
         },
       }
 
