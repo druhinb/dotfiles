201ed89506c4603134290def496f3b6d72dca8c1
Druhin Bhowal <druhinb@dropzone.ai>
2025-10-30 16:42:36 -0700

    jetbrains mono on wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "nordfox"
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 19
 
 config.enable_tab_bar = false
