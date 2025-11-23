b91d1b9e5138ddfb6b6601b05806b9cb2eccdaac
Druhin Bhowal <druhin.bhowal@outlook.com>
2025-11-22 23:51:58 -0800

    fix: fix snacks configuration

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -8,29 +8,54 @@ return {
     quickfile = { enabled = true }, -- Faster file opening
     statuscolumn = { enabled = true }, -- Better status column (git signs, folds)
     words = { enabled = true }, -- Auto-highlight word references (like vim-illuminate)
-    
+
     -- "Tools" - Useful tools to have on hand
-    lazygit = { enabled = false }, -- Best lazygit integration
+    lazygit = { enabled = true }, -- Best lazygit integration
     scratch = { enabled = true }, -- Scratch buffers
     terminal = { enabled = false }, -- Simple terminal toggle
     zen = { enabled = false }, -- Zen mode
-    
-    dashboard = { enabled = false }, 
-    indent = { enabled = false }, 
+
+    dashboard = { enabled = false },
+    indent = { enabled = false },
     notifier = { enabled = false },
-    picker = { enabled = false }, 
+    picker = { enabled = false },
     input = { enabled = false },
     scope = { enabled = false },
     scroll = { enabled = false },
   },
   keys = {
-    
-    { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
-    { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
-    
-    { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
-    
-    { ']]', function() Snacks.words.jump(vim.v.count1) end, desc = 'Next Reference', mode = { 'n', 't' } },
-    { '[[', function() Snacks.words.jump(-vim.v.count1) end, desc = 'Prev Reference', mode = { 'n', 't' } },
+    -- LazyGit
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+
+    -- Scratch Buffer
+    {
+      '<leader>.',
+      function()
+        Snacks.scratch()
+      end,
+      desc = 'Toggle Scratch Buffer',
+    },
+    {
+      '<leader>S',
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = 'Select Scratch Buffer',
+    },
+
+    -- Better Buffer Delete (keeps layout intact)
+    {
+      '<leader>bd',
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = 'Delete Buffer',
+    },
   },
 }
